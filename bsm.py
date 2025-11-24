Added greeks and implied vol info for 30 day at-the-money call option on SPY. Corrected gamma calculation.

bsm.py
@@ -27,11 +27,11 @@ def put_delta(S, strike, time, r, sigma):
 
 
 def call_gamma(S, strike, time, r, sigma):
-    return scipy.stats.norm.pdf(d1(S, strike, time, r, sigma), 0.0, 1.0) / (sigma * np.sqrt(time))
+    return scipy.stats.norm.pdf(d1(S, strike, time, r, sigma), 0.0, 1.0) / (S*(sigma * np.sqrt(time)))
 
 
 def put_gamma(S, strike, time, r, sigma):
-    return scipy.stats.norm.pdf(d1(S, strike, time, r, sigma), 0.0, 1.0) / (sigma * np.sqrt(time))
+    return scipy.stats.norm.pdf(d1(S, strike, time, r, sigma), 0.0, 1.0) / (S*(sigma * np.sqrt(time)))
 
 
 def call_theta(S, strike, time, r, sigma):
@@ -56,3 +56,20 @@ def call_greeks(S, strike, time, r, sigma):
 
 def put_greeks(S, strike, time, r, sigma):
     return put_delta(S, strike, time, r, sigma), put_gamma(S, strike, time, r, sigma), put_vega(S, strike, time, r, sigma), put_theta(S, strike, time, r, sigma), put_rho(S, strike, time, r, sigma)
+
+
+def call_price(S, strike, time, r, sigma):
+    return S * scipy.stats.norm.cdf(d1(S, strike, time, r, sigma), 0.0, 1.0) - strike * np.exp(-r*time) * scipy.stats.norm.cdf(d2(S, strike, time, r, sigma), 0.0, 1.0)
+
+
+# Calculates Implied Volatility of the Option Contract using the Newton-Raphson Method.
+def call_implied_vol(S, strike, time, r, opt_price, sigma_est, iterations):
+
+    for i in range(iterations):
+        diff = call_price(S, strike, time, r, sigma_est)-opt_price
+        sigma_est -= diff / call_vega(S, strike, time, r, sigma_est)
+        if np.abs(diff) < 0.001:
+            break
+        else:
+            continue
+    return sigma_est